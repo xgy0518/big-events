d6790b3948bc85476dfa583649dfba7589b8acc7
didi <xu15865861655@163.com>
2021-03-13 18:28:43 +0800

    index页面完成

---
+++ big-event/assets/js/index.js
@@ -0,0 +1,41 @@
+$(function () {
+
+  let layer = layui.layer;
+  getUserInfo()
+  function getUserInfo() {
+    axios
+      .get('http://ajax.frontend.itheima.net/my/userinfo', {
+        headers: {
+          Authorization: localStorage.getItem('token'),
+        },
+      })
+      .then((res) => {
+        console.log(res)
+
+        let info = res.data.data
+        let name = info.nickname || info.username
+
+        $('#welcome').text('欢迎' + name)
+
+        if (info.user_pic) {
+          $('.layui-nav-img').attr('src', info.user_pic).show()
+
+          $('.text-avatar-box').hide()
+        } else {
+          $('.text-avatar-box').show().children().text(name[0].toUpperCase())
+
+          $('.layui-nav-img').hide()
+        }
+      })
+  }
+
+  // 点击退出
+  $('#btnLogout').click(function(){
+    layer.confirm('你确定要删除吗', {icon: 3, title:'提示'}, function(index){
+      
+      localStorage.removeItem('token');
+      layer.close(index);
+      location.href = 'login.html'
+    });
+  })
+})
